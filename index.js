9ecc808e4c412b11342673cea114e8dda40078c2
Amarjit Kumar <amarjitkr0511@gmail.com>
2023-03-23 22:57:10 +0530

    Worked on HTML and CSS part

---
+++ index.js
@@ -0,0 +1,9 @@
+body {
+    margin: 0;
+    display: flex;
+    justify-content: clearInterval;
+    height: 100vh;
+    align-items: clearInterval;
+    background-color: white:
+    font-family:cursive;
+}
